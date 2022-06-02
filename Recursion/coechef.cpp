f74271a87013b1df59c586660a494fe84eb84ac4
Dron <dronr19@gmail.com>
2022-06-02 19:17:16 +0530

    dp

---
+++ Recursion/coechef.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int a;
+        cin>>a;
+        vector<int>vec(a);
+        for(int i=0;i<a;i++){
+            cin>>vec[i];
+        }
+        
+    }
+    return 0;
+}
--- Recursion/letsgotoday.cpp
+++ Recursion/letsgotoday.cpp
@@ -3,26 +3,36 @@ using namespace std;
 
 typedef long long ll;
 
-void subssequence(vector<int>&vec,vector<int>&curr,int index){
+void printallpaths(vector<vector<int>>&vec,string str,int row,int col){
   
-   if(index==vec.size()){
-       for(auto ele:vec)
-           cout<<ele<<" ";
-    cout<<endl;
-           return;
-   }
-    for(int i =index;i<vec.size();i++){
-    swap(vec[index],vec[i]);
-    subssequence(vec,curr,index+1);
-      swap(vec[index],vec[i]);;
+     
+    if(row==vec.size()-1 && col==vec[0].size()-1){
+        cout<<str;
+        cout<<endl;
+        return;
     }
-    
-
+    if(vec[row][col]==0)return;
+    vec[row][col]=0;
+    if(row<vec.size()-1){
+        printallpaths(vec,str+'D',row+1,col);
+        
+    }
+    if(row>0){
+        printallpaths(vec,str+'U',row-1,col);
+  
+    }
+    if(col<vec[0].size()-1){
+        printallpaths(vec,str+'R',row,col+1);
+      
+    }
+    if(col>0){
+        printallpaths(vec,str+'L',row,col-1);
+       
+    }
+     vec[row][col]=1;
 }
 int main(){
-    vector<int>vec={1,2,3};
-    vector<int>ans;
-    sort(vec.begin(),vec.end());
-    subssequence(vec,ans,0);
+    vector<vector<int>>board={{1,0,0,0},{1,1,0,1},{1,1,0,0},{0,1,1,1}};
+    printallpaths(board,"",0,0);
     return 0;
 }
